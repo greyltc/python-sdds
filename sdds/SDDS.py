8925ec4f23827a85af42e5677889a2866cd3778e
Grey Christoforo <grey@christoforo.net>
2017-02-21 00:21:44 +0000

    working on parsing header lines

--- sdds/SDDS.py
+++ sdds/SDDS.py
@@ -1,11 +1,15 @@
 from . import eprint
 import gzip
+import f90nml
+import io
+import re
 
 class SDDS:
   def __init__(self, fileName=None):
     self.description = ["", ""]
     self.version = None # SDDS format version
-    self.gzipped = None # is the underlying file gzipped?    
+    self.gzipped = None # is the underlying file gzipped?
+    self.nPages = None # number of data pages
     if fileName is not None:
       fp = open(fileName,'rb') # open the file for reading in binary mode
       first5 = fp.read(5) # let's inspect the first 5 bytes
@@ -40,6 +44,26 @@ class SDDS:
         f = fp.read()
       fp.close()
       
-      lines = f.split(b'\n')
-      for line in lines:
-        print (line)
+      partitioned = f.partition(b'\n!') # split out the header
+      header = partitioned[0] 
+      headerLines = header.splitlines() # split the header into lines
+      del headerLines[0] # we already checked that this is 'SDDS1'
+      
+      # parse each header line using f90nml
+      nmlStream =  io.StringIO() # we'll write our header lines to this file-like object so that f90nml can read them
+      for hLine in headerLines:
+        string = hLine.decode("utf-8").replace('=%',"='%'") # hack to fix a bug in f90nml, see https://github.com/marshallward/f90nml/issues/42
+        nmlStream.write(string)
+        nmlStream.truncate() # delete any previous longer line leftover
+        nmlStream.seek(0) # go back to the start of the file to get ready to read
+        nameList = f90nml.read(nmlStream) # parse the header line with f90nml
+        nmlStream.seek(0) # get ready to write the next header line
+        print('Header Row: ' + str(nameList))
+      
+      dataPages = b'!' + partitioned[2] # reattach the !
+      dataPages = re.split(b'! page number [0-9]*\n', dataPages) # split up each page
+      del dataPages[0] # remove the empty index from re.split
+      self.nPages = len(dataPages)
+      
+      for page in dataPages:
+        print('Data Page: ' + str(page))
